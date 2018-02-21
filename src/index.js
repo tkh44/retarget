2977c1790b3c8faea6e6508764f027d48c7614b2
Kye Hohenberger <kye@sideway.com>
2018-02-20 23:24:50 -0700

    ⚠️

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import 'setimmediate';
+import 'setimmediate'
 
 // Used to transfer object from a `toPrimitive` call
 // to the recieving retarget
-let transfer;
+let transfer
 
 const removeTransferOnNextTick = () => {
   setImmediate(() => {
-    transfer = undefined;
-  });
-};
+    transfer = undefined
+  })
+}
 
 // Create an instance fn and attach
 // a path array
 const createInstance = prop => {
-  const fn = obj => fn.path.reduce((last, part) => last && last[part], obj);
-  fn.toString = () => `retarget.${fn.path.join('.')}`;
-  fn.path = [prop];
-  return fn;
-};
+  const fn = obj => fn.path.reduce((last, part) => last && last[part], obj)
+  fn.toString = () => `retarget.${fn.path.join('.')}`
+  fn.path = [prop]
+  return fn
+}
 
 // Handler for each instance
 const retargetHandler = {
@@ -27,36 +27,69 @@ const retargetHandler = {
     // retarget.a[retarget.b];
     // should be the same as retarget.a.b
     if (prop === Symbol.toPrimitive) {
-      transfer = target.path;
-      removeTransferOnNextTick();
-      return target.toString;
+      transfer = target.path
+      removeTransferOnNextTick()
+      return target.toString
     }
 
     if (prop === 'toString') {
-      return target.toString;
+      return target.toString
     }
 
     // If there is a transfer use it
     if (transfer) {
-      target.path = target.path.concat(transfer);
-      transfer = undefined; // remove it afterwards
+      target.path = target.path.concat(transfer)
+      transfer = undefined // remove it afterwards
     } else {
-      target.path.push(prop);
+      target.path.push(prop)
     }
 
-    return reciever;
+    return reciever
   },
 
   apply(target, value, args) {
-    return target(args[0]);
+    return target(args[0])
   }
-};
+}
 
 // Create Retarget Instances
 const retargetCreator = {
   get(target, prop) {
-    return new Proxy(createInstance(prop), retargetHandler);
+    return new Proxy(createInstance(prop), retargetHandler)
+  },
+  apply(target, value, args) {
+    const woven = target(...args)
+      .filter(Boolean)
+      .map(createInstance)
+    console.log('woven', woven)
+    return woven
   }
-};
+}
 
-export default new Proxy({}, retargetCreator);
+function interleave(strings, ...exprs) {
+  // return strings.join()
+  return strings.reduce((accum, s, i) => {
+    return accum.concat(s.split('.'), exprs[i] && handleInterpolation(exprs[i]))
+  }, [])
+}
+
+function handleInterpolation(interpolation) {
+  switch (typeof interpolation) {
+    case 'boolean':
+      return ''
+    case 'function':
+      if (typeof interpolation['__selector'] === 'function')
+        return interpolation['__selector']()
+
+      return handleInterpolation.call(this)
+    case 'object':
+      if (Array.isArray(interpolation)) {
+        return interpolation.map(handleInterpolation)
+      }
+    // eslint-disable-next-line no-fallthrough
+    default:
+      return interpolation ? interpolation.toString() : ''
+  }
+}
+
+export default new Proxy(interleave, retargetCreator)
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
-import retarget from '../src/index';
+import retarget from '../src/index'
 
 test('basic', () => {
-  const selector = retarget.profile.name.last;
+  const selector = retarget.profile.name.last
 
   const STATE = {
     profile: {
@@ -10,10 +10,10 @@ test('basic', () => {
         last: 'Jennings'
       }
     }
-  };
+  }
 
-  expect(selector(STATE)).toMatchSnapshot();
-});
+  expect(selector(STATE)).toMatchSnapshot()
+})
 
 test('nested', () => {
   const STATE2 = {
@@ -35,12 +35,12 @@ test('nested', () => {
         }
       }
     }
-  };
+  }
 
-  const userSelector = retarget.users;
-  const selector2 = userSelector[2].profile.name.last;
-  expect(selector2(STATE2)).toMatchSnapshot();
-});
+  const userSelector = retarget.users
+  const selector2 = userSelector[2].profile.name.last
+  expect(selector2(STATE2)).toMatchSnapshot()
+})
 
 test('fun fun functions', () => {
   const STATE = {
@@ -54,16 +54,16 @@ test('fun fun functions', () => {
         }
       }
     }
-  };
+  }
 
-  const lastNameSelector = retarget.profile.name.last;
+  const lastNameSelector = retarget.profile.name.last
 
-  const createUserSelector = id => retarget.users[id][lastNameSelector];
+  const createUserSelector = id => retarget.users[id][lastNameSelector]
 
-  const userSelector = createUserSelector(1);
+  const userSelector = createUserSelector(1)
 
-  expect(userSelector(STATE)).toMatchSnapshot();
-});
+  expect(userSelector(STATE)).toMatchSnapshot()
+})
 
 test('compound', () => {
   const STATE3 = {
@@ -87,7 +87,7 @@ test('compound', () => {
         }
       }
     }
-  };
+  }
   // I can't figure out how to dedupe selectors
   // If you use the 2 above one after the other you will get ['profile', 'name', 'last'] twice in the key
 
@@ -95,23 +95,127 @@ test('compound', () => {
   // looking for a match and then overwriting (left to right) from there might work
   // need to do some sort of check before doing this check as it is stupid innefficent
   // unless we know there is a hit
-  const selectorA = retarget.users[1];
-  const selectorB = retarget.profile;
-  const selectorC = retarget.first;
+  const selectorA = retarget.users[1]
+  const selectorB = retarget.profile
+  const selectorC = retarget.first
   const compoundSelector =
-    retarget.entities[selectorA][selectorB].name[selectorC];
+    retarget.entities[selectorA][selectorB].name[selectorC]
 
-  expect(compoundSelector(STATE3)).toMatchSnapshot();
-});
+  expect(compoundSelector(STATE3)).toMatchSnapshot()
+})
 
 test('debug output', () => {
-  const selectorA = retarget.users[1];
-  const selectorB = retarget.profile;
-  const selectorC = retarget.first;
+  const selectorA = retarget.users[1]
+  const selectorB = retarget.profile
+  const selectorC = retarget.first
   const compoundSelector =
-    retarget.entities[selectorA][selectorB].name[selectorC];
+    retarget.entities[selectorA][selectorB].name[selectorC]
 
   expect(compoundSelector.toString()).toEqual(
     'retarget.entities.users.1.profile.name.first'
-  );
-});
+  )
+})
+
+test('basic', () => {
+  const selector = retarget`profile.name.last`
+
+  const arraySelector = retarget(['profile', 'name', 'first'])
+  const STATE = {
+    profile: {
+      name: {
+        first: 'Waylon',
+        last: 'Jennings'
+      }
+    }
+  }
+
+  expect(selector(STATE)).toMatchSnapshot()
+  expect(arraySelector(STATE)).toMatchSnapshot()
+})
+
+test('nested', () => {
+  const STATE2 = {
+    users: {
+      '1': {
+        profile: {
+          name: {
+            first: 'Waylon',
+            last: 'Jennings'
+          }
+        }
+      },
+      '2': {
+        profile: {
+          name: {
+            first: 'Waylon',
+            last: 'Jennings'
+          }
+        }
+      }
+    }
+  }
+
+  const userSelector = retarget`users`
+  const selector2 = retarget`${userSelector}.2.profile.name.last`
+  expect(selector2(STATE2)).toMatchSnapshot()
+})
+
+test('fun fun functions', () => {
+  const STATE = {
+    users: {
+      '1': {
+        profile: {
+          name: {
+            first: 'Waylon',
+            last: 'Jennings'
+          }
+        }
+      }
+    }
+  }
+
+  const lastNameSelector = retarget`profile.name.last`
+
+  const createUserSelector = id => retarget`users.${id}${lastNameSelector}`
+
+  const userSelector = createUserSelector(1)
+
+  expect(userSelector(STATE)).toMatchSnapshot()
+})
+
+test('compound', () => {
+  const STATE3 = {
+    entities: {
+      users: {
+        '1': {
+          profile: {
+            name: {
+              first: 'Waylon',
+              last: 'Jennings'
+            }
+          }
+        },
+        '2': {
+          profile: {
+            name: {
+              first: 'Waylon',
+              last: 'Jennings'
+            }
+          }
+        }
+      }
+    }
+  }
+  // I can't figure out how to dedupe selectors
+  // If you use the 2 above one after the other you will get ['profile', 'name', 'last'] twice in the key
+
+  // I think going from right to left with the head of the new key array
+  // looking for a match and then overwriting (left to right) from there might work
+  // need to do some sort of check before doing this check as it is stupid innefficent
+  // unless we know there is a hit
+  const selectorA = retarget`users.1`
+  const selectorB = retarget`profile`
+  const selectorC = retarget`first`
+  const compoundSelector = retarget`entities.${selectorA}${selectorB}.name.${selectorC}`
+  expect(compoundSelector(STATE3)).toMatchSnapshot()
+})
