9949712137f58bd08832a0be89eed8c8b5a780a7
Fabrice Weinberg <Fabrice@weinberg.me>
2018-02-22 22:02:04 +0100

    add basic tests for mixed proxy/template usage

---
+++ test/mixed.test.js
@@ -0,0 +1,31 @@
+import retarget from '../src/index';
+
+test('first use tagged template', () => {
+  const selector = retarget`profile.name.${retarget.last}`;
+
+  const STATE = {
+    profile: {
+      name: {
+        first: 'Waylon',
+        last: 'Jennings'
+      }
+    }
+  };
+
+  expect(selector(STATE)).toMatchSnapshot();
+});
+
+test('first use proxy', () => {
+  const selector = retarget.profile`name.${retarget.last}`;
+
+  const STATE = {
+    profile: {
+      name: {
+        first: 'Waylon',
+        last: 'Jennings'
+      }
+    }
+  };
+
+  expect(selector(STATE)).toMatchSnapshot();
+});
